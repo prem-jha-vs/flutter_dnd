efc4fa303ff9b233bdead3a9570398a6319ff664
prem-jha-vs <157302866+prem-jha-vs@users.noreply.github.com>
2025-05-16 10:55:06 +0530

    Update FlutterDndPlugin.java

--- android/src/main/java/com/github/sathish76/flutter_dnd/FlutterDndPlugin.java
+++ android/src/main/java/com/github/sathish76/flutter_dnd/FlutterDndPlugin.java
@@ -1,65 +1,40 @@
 package com.github.sathish76.flutter_dnd;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.content.Intent;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-
-import android.app.NotificationManager;
-import android.content.Context;
-import android.os.Bundle;
-import android.util.Log;
-import android.content.Intent;
-import android.provider.Settings;
-
-/**
- * FlutterDndPlugin
- */
 
 public class FlutterDndPlugin implements FlutterPlugin, MethodCallHandler {
-
-    private static NotificationManager notificationManager;
-    private static Context context;
+    private MethodChannel channel;
+    private Context applicationContext;
+    private NotificationManager notificationManager;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        context = flutterPluginBinding.getApplicationContext();
-        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(),
-                "flutter_dnd");
-        channel.setMethodCallHandler(new FlutterDndPlugin());
-    }
-
-    // This static function is optional and equivalent to onAttachedToEngine. It
-    // supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new
-    // Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith
-    // to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith
-    // will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both
-    // be defined
-    // in the same class.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_dnd");
-        channel.setMethodCallHandler(new FlutterDndPlugin());
+        applicationContext = flutterPluginBinding.getApplicationContext();
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_dnd");
+        channel.setMethodCallHandler(this);
+        notificationManager = (NotificationManager) applicationContext.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        if (!isAboveMarshmello()) {
-            result.error("ERROR: INCOMPATIBLE_ANDROID_VERSION", "This methods required android version above 23", null);
+        if (!isAboveMarshmallow()) {
+            result.error("ERROR_INCOMPATIBLE_ANDROID_VERSION", 
+                "This method requires Android version 23 (Marshmallow) or higher", null);
             return;
         }
+
         switch (call.method) {
             case "isNotificationPolicyAccessGranted":
                 result.success(isNotificationPolicyAccessGranted());
@@ -69,8 +44,12 @@ public class FlutterDndPlugin implements FlutterPlugin, MethodCallHandler {
                 result.success(null);
                 break;
             case "setInterruptionFilter":
-                int interruptionFilter = call.arguments();
-                result.success(setInterruptionFilter(interruptionFilter));
+                if (call.arguments != null) {
+                    int interruptionFilter = (int) call.arguments;
+                    result.success(setInterruptionFilter(interruptionFilter));
+                } else {
+                    result.error("ERROR_NULL_ARGUMENT", "Interruption filter argument cannot be null", null);
+                }
                 break;
             case "getCurrentInterruptionFilter":
                 result.success(getCurrentInterruptionFilter());
@@ -81,10 +60,11 @@ public class FlutterDndPlugin implements FlutterPlugin, MethodCallHandler {
         }
     }
 
-    private boolean isAboveMarshmello() {
-        return android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M;
+    private boolean isAboveMarshmallow() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private boolean isNotificationPolicyAccessGranted() {
         return notificationManager.isNotificationPolicyAccessGranted();
     }
@@ -92,9 +72,10 @@ public class FlutterDndPlugin implements FlutterPlugin, MethodCallHandler {
     private void gotoPolicySettings() {
         Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+        applicationContext.startActivity(intent);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private boolean setInterruptionFilter(int interruptionFilter) {
         if (notificationManager.isNotificationPolicyAccessGranted()) {
             notificationManager.setInterruptionFilter(interruptionFilter);
@@ -103,12 +84,13 @@ public class FlutterDndPlugin implements FlutterPlugin, MethodCallHandler {
         return false;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private int getCurrentInterruptionFilter() {
         return notificationManager.getCurrentInterruptionFilter();
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
     }
-
 }
